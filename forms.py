bdbb86149e4a2c37428ff6a4cf579bf51f7f7d57
Mark <mdlynn03@icloud.com>
2025-06-01 04:37:10 -0400

    added required registration fields

--- forms.py
+++ forms.py
@@ -1,23 +1,40 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, Regexp
 
 from models import User, UserProfile, Address, SocialProfile, EducationHistory, WorkExperience, Skill
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired(), EqualTo('password')]
-    )
+    first_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=50)])
+    last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=50)])
     phone_number = StringField('Phone Number', validators=[DataRequired(), Length(max=20)])
     street_address = StringField('Street Address', validators=[Length(max=100)])
     city = StringField('City', validators=[Length(max=50)])
     state = StringField('State', validators=[Length(max=50)])
     zip_code = StringField('Zip Code', validators=[Length(max=10)])
     country = StringField('Country', validators=[Length(max=50)])
+    password = PasswordField('Password',
+        validators=[
+        DataRequired(),
+        Length(min=8,max=30),
+        Regexp(
+            regex=r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$',
+            message="Password must contain uppercase, lowercase, digits, and special characters."
+            )
+        ]
+    )
+    confirm_password = PasswordField(
+        'Confirm Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password'),
+            Length(min=8,max=30)
+        ]
+    )
+
+
     bio = TextAreaField('Bio', validators=[Length(max=300)])
     hobbies = TextAreaField('Hobbies', validators=[Length(max=200)])
 
